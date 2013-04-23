8e30aedb29976666750c5a864780ceb878b789fa
Mat Lipe <Mat Lipe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-23 17:17:15 +0000

    Version 4.1.2 which adds more filters to allow for simpler addons
    git-svn-id: https://plugins.svn.wordpress.org/advanced-sidebar-menu/tags/4.1.2@702373 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/advancedSidebarMenu.php
@@ -0,0 +1,276 @@
+<?php 
+
+
+         /**
+          * These Functions are Specific to the Advanced Sidebar Menu
+          * @author Mat Lipe
+          * @since 4.23.13
+          * 
+          * @package Advanced Sidebar Menu
+          */
+class advancedSidebarMenu{
+      var $instance; //The widget instance 
+      var $top_id; //Either the top cat or page
+      var $exclude;
+      var $ancestors; //For the category ancestors
+      var $count = 1; //Count for grandchild levels
+      var $order_by;
+         
+         
+      /**   
+       * Removes the closing </li> tag from a list item to allow for child menus inside of it
+       * 
+       * @param string $item - an <li></li> item
+       * @return string|bool
+       * @since 4.7.13
+       */
+      function openListItem($item = false){
+          if( !$item ) return false;
+          
+          return substr(trim($item), 0, -5);
+      }   
+         
+      /**
+       * The Old way of doing thing which displayed all 3rd levels and below when on a second level page
+       * This is only here for people afraid of change who liked the old way of doing things
+       * 
+       * @uses used in views -> page_list.php when legacy mode is checked in widget
+       * @since 4.7.13
+       */   
+      function grandChildLegacyMode($pID ){
+          #-- if the link that was just listed is the current page we are on
+            if( !$this->page_ancestor( $pID ) ) return;
+
+                //Get the children of this page
+                $grandkids = $this->page_children($pID->ID );                
+                if( $grandkids ){
+                    #-- Create a new menu with all the children under it
+                    $content .= '<ul class="grandchild-sidebar-menu">';
+                            $content .= wp_list_pages("post_type=".$this->post_type."&sort_column=$this->order_by&title_li=&echo=0&exclude=".$this->instance['exclude']."&child_of=".$pID->ID);
+
+                    $content .= '</ul>';
+                }
+                
+                return $content;
+      }    
+                  
+         
+     /**   
+      * Displays all the levels of the Grandchild Menus
+      * 
+      * Will run until there are no children left for the current page's hyercy
+      * Only displays the pages if we are on a child or grandchild page of the Id sent 
+      * which at the time of creation comes from the child level pages
+      * 
+      * 
+      * @uses called by the widget view page_list.php
+      * @since 4.0
+      * 
+      * @since 4.7.13
+      */
+     function displayGrandChildMenu($page){
+        static $count = 0;
+        $count++;
+
+        //If the page sent is not a child of the current page
+        if( !$this->page_ancestor($page) ) return;
+        
+        //if there are no children of the current page bail
+        if( !$children = $this->page_children($page->ID) ) return;
+
+       $content .= sprintf('<ul class="grandchild-sidebar-menu level-%s children">',$count );
+        foreach( $children as $child ){
+            
+
+            $args = array(
+                  'post_type' => $this->post_type,
+                  'sort_column' => $this->order_by,
+                  'title_li'    => '',
+                  'echo'        => 0,
+                  'depth'       => 1,
+                  'exclude'     => join(',',$this->exclude),
+                  'include'     => $child->ID
+                  );
+                  
+            $content .= $this->openListItem(wp_list_pages($args));
+
+            //If this newly outputed child is a direct parent of the current page
+            if( $this->page_ancestor($child) ){
+               $content .= $this->displayGrandChildMenu($child);
+            }
+            
+            $content .= '</li>';
+        
+       }
+       $content .= '</ul>';
+       
+       return $content; 
+        
+    }
+         
+         
+         
+         
+         
+         /**
+          * Adds the class for current page item etc to the page list when using a custom post type
+          * @param array $css the currrent css classes
+          * @param obj $this_menu_item the page being checked
+          * @return array
+          * @since 10.10.12
+          */
+    function custom_post_type_css($css, $this_menu_item){
+             global $post;
+             if ( isset($post->ancestors) && in_array($this_menu_item->ID, (array)$post->ancestors) ){
+                 $css[] = 'current_page_ancestor';
+             }
+             if ( $this_menu_item->ID == $post->ID ){
+                 $css[] = 'current_page_item';
+             
+             } elseif ($this_menu_item->ID == $post->post_parent ){
+                 $css[] = 'current_page_parent';
+             }
+             return $css;
+     }
+         
+
+    /**
+     * 
+     * IF this is a top level category
+     * @param obj $cat the cat object
+     */
+    function first_level_category( $cat ){
+        if( !in_array($cat->cat_ID, $this->exclude) && $cat->parent == $this->top_id){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * If the cat is a second level cat
+     * @param obj $cat the cat
+     * @since 10.12.12
+     */
+    function second_level_cat( $child_cat ){
+        //if this is the currrent cat or a parent of the current cat
+        if( $child_cat->cat_ID == get_query_var('cat' ) || in_array( $child_cat->cat_ID, $this->ancestors )){
+            $all_children = array();
+            $all_children = get_categories( array( 'child_of' => $child_cat->cat_ID ) );
+            if( !empty( $all_children ) ){
+                return true;
+            } else {
+                return false;
+            }
+            
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * Determines if all the children should be included
+     * @since 7/16/12
+     * @return bool
+     */
+    function display_all(){
+        if( $this->instance['display_all'] == 'checked' ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * 
+     * Returns and array of all the children of a page
+     * @param int $pID the id of the page
+     * @since 4.8.13
+     */
+    function page_children( $pID ){
+        global $wpdb, $table_prefix;
+        return $wpdb->get_results( "SELECT ID FROM ".$table_prefix."posts WHERE post_parent = ".$pID." AND post_status='publish' ORDER By ".$this->order_by );
+        
+    }
+    
+    /**
+     * 
+     * Determines if this is an ancestor or the current post
+     * @param obj $pID the post object
+     * @since 7/19/12
+     */
+    function page_ancestor( $pID ){
+        global $post;
+        if($pID->ID == $post->ID or $pID->ID == $post->post_parent or @in_array($pID->ID, $post->ancestors) ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    
+    /**
+     * Determines if the parent page or cat should be included
+     * @since 7/16/12
+     * @return bool
+     */
+    function include_parent(){
+        if( ($this->instance['include_parent'] == 'checked') && (!in_array($this->top_id, $this->exclude)) ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+   
+    /**
+     * Echos the title of the widget to the page
+     * @since 4.7.13
+     */
+    function title(){
+        if( $this->instance['title'] != '' ){
+            
+            $title = apply_filters('widget_title', $this->instance['title'], $this->args, $this->instance );
+            
+            echo '<h4 class="widgettitle">' . $title . '</h4>';
+        }
+        
+    }
+    
+    
+    /**
+     * 
+     * Checks is this id is excluded or not
+     * @param int $id the id to check
+     * @return bool
+     */
+    function exclude( $id ){
+        if( !in_array( $id, $this->exclude ) ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+
+
+    /**
+    * Allows for Overwritting files in the child theme
+    * @since 4.23.13
+    * @param string $file the name of the file to overwrite
+    */
+         
+     static function file_hyercy( $file, $legacy = false ){
+        if ( $theme_file = locate_template(array('advanced-sidebar-menu/'.$file)) ) {
+            $file = $theme_file;
+        } elseif( $legacy ){
+            $file = ADVANCED_SIDEBAR_LEGACY_DIR . $file;
+        } else {
+            $file = ADVANCED_SIDEBAR_VIEWS_DIR . $file;
+        }
+        return apply_filters( 'advanced_sidebar_menu_view_file', $file, $legacy );
+    
+    }
+
+} //End class
+
---
+++ widgets/category.widget.php
@@ -0,0 +1,271 @@
+<?php 
+
+
+          /**
+           * Creates a Widget of parent Child Categories
+           * 
+           * @author mat lipe
+           * @since 4.23.13
+           * @package Advanced Sidebar Menu
+           *
+           */
+class advanced_sidebar_menu_category extends WP_Widget {
+
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+    // This decides the name of the widget
+    function __construct() {
+                /* Widget settings. */
+        $widget_ops = array( 'classname' => 'advanced-sidebar-menu advanced-sidebar-category', 'description' => 'Creates a menu of all the Categories using the child/parent relationship' );
+        $control_ops = array( 'width' => 290 );
+        /* Create the widget. */
+        $this->WP_Widget( 'advanced_sidebar_menu_category', 'Advanced Sidebar Categories Menu', $widget_ops, $control_ops );
+        }
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+	  // this creates the widget form for the dashboard
+	function form( $instance ) {
+				  	//	 require( ADVANCED_SIDEBAR_DIR . 'advanced-sidebar-menu.js' );
+			?>
+			 <p> Title <br>
+             <input id="<?php echo $this->get_field_name('title'); ?>" 
+            	name="<?php echo $this->get_field_name('title'); ?>" size="50" type="text" value="<?php echo $instance['title']; ?>"/></p>
+			
+			
+            <p> Include Parent Category <input id="<?php echo $this->get_field_name('include_parent'); ?>" 
+            	name="<?php echo $this->get_field_name('include_parent'); ?>" type="checkbox" value="checked" 
+            	<?php echo $instance['include_parent']; ?>/></p>
+			
+            			
+			<p> Include Parent Even With No Children <input id="<?php echo $this->get_field_name('include_childless_parent'); ?>"
+			name="<?php echo $this->get_field_name('include_childless_parent'); ?>" type="checkbox" value="checked" 
+					<?php echo $instance['include_childless_parent']; ?>/></p>
+					
+			<p> Use this plugins styling <input id="<?php echo $this->get_field_name('css'); ?>"
+			name="<?php echo $this->get_field_name('css'); ?>" type="checkbox" value="checked" 
+					<?php echo $instance['css']; ?>/></p>
+					
+			<p> Display Categories on Single Posts <input id="<?php echo $this->get_field_name('single'); ?>"
+			name="<?php echo $this->get_field_name('single'); ?>" type="checkbox" value="checked" 
+			onclick="javascript:asm_reveal_element( 'new-widget-<?php echo $this->get_field_name('new_widget'); ?>' )"
+					<?php echo $instance['single']; ?>/></p>	
+			
+			<span id="new-widget-<?php echo $this->get_field_name('new_widget'); ?>" style="<?php 
+                  if( $instance['single'] == checked ){
+                  	echo 'display:block';
+                  } else {
+                  	echo 'display:none';
+                  } ?>"> 		
+				 <p>Display Each Single Post's Category 
+			 		<select id="<?php echo $this->get_field_name('new_widget'); ?>" 
+            				name="<?php echo $this->get_field_name('new_widget'); ?>">
+            		<?php 
+            			if( $instance['new_widget'] == 'widget' ){
+            				echo '<option value="widget" selected> In a new widget </option>';
+            				echo '<option value="list"> In another list in the same widget </option>';
+            			} else {
+            				echo '<option value="widget"> In a new widget </option>';
+            				echo '<option value="list" selected> In another list in the same widget </option>';
+            			}
+            		
+            		?></select>
+            	 </p>
+            </span>
+         
+            	
+					
+			<p> Categories to Exclude, Comma Separated:<input id="<?php echo $this->get_field_name('exclude'); ?>" 
+            	name="<?php echo $this->get_field_name('exclude'); ?>" type="text" value="<?php echo $instance['exclude']; ?>"/></p>
+            
+            
+            <p> Legacy Mode: (use pre 4.0 structure and css) <input id="<?php echo $this->get_field_name('legacy_mode'); ?>"
+            name="<?php echo $this->get_field_name('legacy_mode'); ?>" type="checkbox" value="checked" 
+                    <?php echo $instance['legacy_mode']; ?>/>
+            </p>    
+                
+            
+            
+            	
+            <p> Always Display Child Categories <input id="<?php echo $this->get_field_name('display_all'); ?>" 
+            	name="<?php echo $this->get_field_name('display_all'); ?>" type="checkbox" value="checked" 
+            	onclick="javascript:asm_reveal_element( 'levels-<?php echo $this->get_field_name('levels'); ?>' )"
+            	<?php echo $instance['display_all']; ?>/></p>
+            
+            <span id="levels-<?php echo $this->get_field_name('levels'); ?>" style="<?php 
+                  if( $instance['display_all'] == checked ){
+                  	echo 'display:block';
+                  } else {
+                  	echo 'display:none';
+                  } ?>"> 
+            <p> Levels to Display <select id="<?php echo $this->get_field_name('levels'); ?>" 
+            name="<?php echo $this->get_field_name('levels'); ?>">
+            <?php 
+            	for( $i= 1; $i<6; $i++ ){
+            		if( $i == $instance['levels'] ){
+            			echo '<option value="'.$i.'" selected>'.$i.'</option>';
+            		} else {
+            			echo '<option value="'.$i.'">'.$i.'</option>';
+            		}
+            	} 
+            	echo '</select></p></span>';
+                
+                
+           do_action('advanced_sidebar_menu_category_widget_form', $instance );        
+                
+		}
+
+#------------------------------------------------------------------------------------------------------------------------------
+	// this allows more than one instance
+
+	function update( $new_instance, $old_instance ) {
+			$instance = $old_instance;
+			$instance['include_childless_parent'] = strip_tags($new_instance['include_childless_parent']);
+			$instance['include_parent'] = strip_tags($new_instance['include_parent']);
+			$instance['exclude'] = strip_tags($new_instance['exclude']);
+			$instance['display_all'] = strip_tags($new_instance['display_all']);
+			$instance['levels'] = strip_tags($new_instance['levels']);
+			$instance['css'] = strip_tags($new_instance['css']);
+			$instance['single'] = strip_tags($new_instance['single']);  //Display on single pages
+			$instance['new_widget'] = strip_tags($new_instance['new_widget']); //Create a new widget for each single category
+			$instance['title'] = strip_tags($new_instance['title']);
+			$instance['legacy_mode'] = strip_tags($new_instance['legacy_mode']);
+			return $instance;
+		}
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Outputs the categories widget to the page
+     * 
+     * @since 4.7.13
+     * @uses loads the views/category_list.php
+     * 
+     * @filters apply_filters('advanced_sidebar_menu_category_widget_output', $content, $args, $instance ); 
+     * 
+     */
+	function widget($args, $instance) {
+		$asm = new advancedSidebarMenu;
+        $asm->instance = $instance;
+        $asm->args = $args;
+        
+        $exclude = explode(',', $instance['exclude']);
+        $asm->exclude = $exclude;
+        
+		extract( $args);
+		#-- Create a usable array of the excluded pages
+	
+		$cat_ids = $already_top = array();
+		$asm_once = $asm_cat_widget_count = false; //keeps track of how many widgets this created
+		$count = null;
+		
+		
+		
+		//If on a single page create an array of each category and create a list for each
+		if( is_single() && ($instance['single'] == 'checked') ){
+			$category_array = get_the_category();
+			foreach( get_the_category() as $id => $cat ){
+				$cat_ids[] = $cat->term_id;
+			}
+			
+		//IF on a category page get the id of the category
+		} elseif( is_category() ){
+		    $cat_ids[] = get_query_var('cat');	
+		}
+
+        //Go through each category there will be only one if this is a category page mulitple possible if this is single
+        foreach( $cat_ids as $cat_id ){
+       		 $cat_ancestors = array ();
+             $cat_ancestors[] = $cat_id ;
+       
+        	do {
+             	$cat_id = get_category($cat_id );
+             	$cat_id = $cat_id->parent;
+             	$cat_ancestors[] = $cat_id ; }
+       		 while ($cat_id );
+       
+            
+       		 //Reverse the array to start at the last
+       		 $cat_ancestors = array_reverse( $cat_ancestors );
+             $asm->ancestors = $cat_ancestors;
+             
+       		 //forget the [0] because the parent of top parent is always 0
+       		 $top_cat = $cat_ancestors[1];
+             $asm->top_id = $top_cat;
+  
+       		 
+       		 //Keeps track or already used top levels so this won't double up
+       		 if( in_array( $top_cat, $already_top ) ){
+       		 	continue;
+       		 }
+       		 $already_top[] = $top_cat;
+       		 
+       
+         	//Check for children
+        	$all_categories = get_categories( array( 'child_of' => $top_cat ) );
+            
+        	//for depreciation
+        	$all = $all_categories; 
+        	
+            //If there are any child categories or the include childless parent is checked
+            if( !empty($all_categories ) || ($instance['include_childless_parent'] == 'checked' && !in_array($top_cat, $exclude))  ){
+        		
+        			
+        			//Creates a new widget for each category the single page has if the options are selected to do so
+					if( !$asm_once || ($instance['new_widget'] == 'widget') ){
+
+						//Start the menu
+                        echo $before_widget;
+						$count++; // To change the id of the widget if there are multiple
+						$asm_once = true;  //There has been a div
+						$close = true; //The div should be closed at the end
+						if($instance['new_widget'] == 'list'){ $close = false;} //If this is a list leave it open for possible late ones
+
+					} else {
+						$close = false;
+					}
+                    
+                    
+                    
+                    $legacy = isset( $instance['legacy_mode'] );
+            
+                    if( $instance['css'] == 'checked' ){
+                        echo '<style type="text/css">';
+                            include( $asm->file_hyercy('sidebar-menu.css', $legacy ) );
+                        echo '</style>';
+                    }
+    
+                    
+
+        			//Bring in the view
+        			require( $asm->file_hyercy( 'category_list.php', $legacy ) );
+                   
+        			echo apply_filters('advanced_sidebar_menu_category_widget_output', $content, $args, $instance );		
+      
+        			if( $close ){
+        				//End the Widget Area
+						  echo $after_widget;
+        			}
+
+      			}  //End if any children or include childless parent
+        } //End of each cat loop
+        
+        
+        //IF we were waiting for all the individual lists to complete
+        if( !$close && $asm_once ){
+        	//End the Widget Area
+					echo $after_widget;
+        	
+        }
+			
+	
+     	     
+	} #== /widget()
+	
+} #== /Clas
